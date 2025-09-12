9caae418a993956be6a0f5a5f36584182674ea71
AryanRogye <161079183+AryanRogye@users.noreply.github.com>
2025-09-12 15:25:28 -0500

    organized app coordinator, now i'm ready to start

--- ComfyMark/App/AppCoordinator.swift
+++ ComfyMark/App/AppCoordinator.swift
@@ -43,20 +43,41 @@ class AppCoordinator {
         self.saving      = saving
         self.screenshotManager = screenshotManager
         
-        self.selectionOverlayCoordinator = SelectionOverlayCoordinator(
-            capture: { [weak self] rect, screen in
+        /// Configure Coordinators
+        self.configureSelectionOverlayCoordinator()
+        self.configureSettingsCoordinator()
+        self.configureComfyMarkCoordinator()
+        self.configureHotKeyCoordinator()
+        
+        /// Starting Our Menu Bar, with Closures, for what happens when we:
+        /// Tap On Settings
+        /// Tap On Start
+        /// Tap on Start with a Image selected in History
+        /// Tap on Crop
+        menuBarCoordinator.start(
+            screenshotManager: screenshotManager,
+            appSettings: appSettings,
+            onSettingsTapped: { [weak self] in
                 guard let self = self else { return }
-                self.takeScreenshotAndShow(rect: rect, on: screen)
+                self.settingsCoordinator.showSettings()
+            },
+            onStartTapped: { [weak self] in
+                guard let self else { return }
+                takeScreenshotAndShow()
+            },
+            onStartTappedImage: {[weak self] image, projectName in
+                guard let self else { return }
+                openComfyMarkWindow(image, projectName: projectName)
+            },
+            onCrop: { [weak self] in
+                guard let self = self else { return }
+                self.selectionOverlayCoordinator.show()
             }
         )
-        self.settingsCoordinator = SettingsCoordinator(
-            windows: windowCoordinator,
-            appSettings: appSettings
-        )
-        self.comfyMarkCoordinator = ComfyMarkCoordinator(
-            windows: windowCoordinator
-        )
-        
+    }
+    
+    // MARK: - HotKey Coordinator
+    private func configureHotKeyCoordinator() {
         self.hotkeyCoordinator = HotKeyCoordinator(
             // Regular Screenshot
             onHotKeyDown: { [weak self] in
@@ -74,68 +95,40 @@ class AppCoordinator {
             onSelectionOverlayUp: {
             }
         )
-        
-        /// Starting Our Menu Bar, with Closures, for what happens when we:
-        /// Tap On Settings
-        /// And
-        /// Tap On Start
-        menuBarCoordinator.start(
-            screenshotManager: screenshotManager,
-            appSettings: appSettings,
-            onSettingsTapped: { [weak self] in
+    }
+    
+    // MARK: - ComfyMark Coordinator
+    private func configureComfyMarkCoordinator() {
+        self.comfyMarkCoordinator = ComfyMarkCoordinator(
+            windows: windowCoordinator
+        )
+    }
+    
+    // MARK: - Settings Coordinator
+    private func configureSettingsCoordinator() {
+        self.settingsCoordinator = SettingsCoordinator(
+            windows: windowCoordinator,
+            appSettings: appSettings
+        )
+    }
+
+    // MARK: - Selection Coordinator
+    private func configureSelectionOverlayCoordinator() {
+        self.selectionOverlayCoordinator = SelectionOverlayCoordinator(
+            capture: { [weak self] rect, screen in
                 guard let self = self else { return }
-                self.settingsCoordinator.showSettings()
-            },
-            onStartTapped: { [weak self] in
-                guard let self else { return }
-                takeScreenshotAndShow()
-            },
-            onStartTappedImage: {[weak self] image, projectName in
-                guard let self else { return }
-                showImage(image, projectName: projectName)
-            },
-            onCrop: { [weak self] in
-                guard let self = self else { return }
-                self.selectionOverlayCoordinator.show()
+                self.takeScreenshotAndShow(rect: rect, on: screen)
             }
-        )
-    }
-    
-    private func showImage(_ image: CGImage, windowID: String = "comfymark-\(UUID().uuidString)") {
-        self.comfyMarkCoordinator.showComfyMark(
-            with: image,
-            export: self.export,
-            saving: self.saving,
-            screenshotManager: screenshotManager,
-            /// Update Last Render Time
-            onLastRenderTimeUpdated: { [weak self] renderTimeMs in
-                guard let self = self else { return }
-                self.menuBarCoordinator.updateRenderTime(renderTimeMs)
-            },
-            windowID: windowID
-        )
-    }
-    
-    private func showImage(_ image: CGImage, projectName: String) {
-        let windowID: String = "comfymark-\(UUID().uuidString)"
-        self.comfyMarkCoordinator.showComfyMark(
-            with: image,
-            export: self.export,
-            saving: self.saving,
-            screenshotManager: screenshotManager,
-            /// Update Last Render Time
-            onLastRenderTimeUpdated: { [weak self] renderTimeMs in
-                guard let self = self else { return }
-                self.menuBarCoordinator.updateRenderTime(renderTimeMs)
-            },
-            windowID: windowID,
-            projectName: projectName
         )
     }
 }
 
-// MARK: - Helpers
+// MARK: - Screenshot
 extension AppCoordinator {
+    private func showImage(_ image: CGImage) {
+        /// Decide How We Want to show The Image, Native way or Fullscreen
+        openComfyMarkWindow(image)
+    }
     /// Function To Take Screenshot Of Specified Screen, this is cuz
     /// When we decide what to show the overlay on THAT is the screen
     /// and if the user changes the mouse, then this wont be valid anymore
@@ -156,4 +149,26 @@ extension AppCoordinator {
             }
         }
     }    
+}
+
+
+// MARK: - Open ComfyMark Window With Image
+extension AppCoordinator {
+    private func openComfyMarkWindow(_ image: CGImage, projectName: String? = nil) {
+        /// Generate A Window ID
+        let windowID: String = "comfymark-\(UUID().uuidString)"
+        self.comfyMarkCoordinator.showComfyMark(
+            with: image,
+            export: self.export,
+            saving: self.saving,
+            screenshotManager: screenshotManager,
+            /// Update Last Render Time, Shown in MenuBar
+            onLastRenderTimeUpdated: { [weak self] renderTimeMs in
+                guard let self = self else { return }
+                self.menuBarCoordinator.updateRenderTime(renderTimeMs)
+            },
+            windowID: windowID,
+            projectName: projectName
+        )
+    }
 }
